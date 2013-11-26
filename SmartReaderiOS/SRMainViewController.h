3fa4849d4afc3809ffb9a5c7689b3b7ca6d416be
Ke Sun <kesun.421@gmail.com>
2013-11-25 16:11:31 -0800

    Added view controllers for secondary table view and main content view.

---
+++ SmartReaderiOS/SRMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SRMainViewController.h
+//  SmartReaderiOS
+//
+//  Created by Ke Sun on 11/25/13.
+//  Copyright (c) 2013 Ke Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRMainViewController : UIViewController
+
+@end
---
+++ SmartReaderiOS/SRSecondaryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SRSecondaryTableViewController.h
+//  SmartReaderiOS
+//
+//  Created by Ke Sun on 11/25/13.
+//  Copyright (c) 2013 Ke Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRSecondaryTableViewController : UITableViewController
+
+@end
