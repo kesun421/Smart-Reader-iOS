dd36dc2a6acb0f05a057a1fa4fcec24bdda4daaf
Ke Sun <kesun.421@gmail.com>
2014-01-23 10:41:25 -0800

    Re-worded local notification text.

--- SmartReaderiOS/SRAppDelegate.m
+++ SmartReaderiOS/SRAppDelegate.m
@@ -115,10 +115,10 @@ typedef void(^BackgroundFetchBlock)(UIBackgroundFetchResult);
     NSString *message = nil;
     
     if (_totalNewCount != 0 && _interestingItemsCount == 0) {
-        message = [NSString stringWithFormat:@"%d new stories to read!", _totalNewCount];
+        message = [NSString stringWithFormat:@"Added %d new articles!", _totalNewCount];
     }
     else if (_totalNewCount != 0 && _interestingItemsCount != 0){
-        message = [NSString stringWithFormat:@"%d new stories to read! %lu stories might be interesting.", _totalNewCount, _interestingItemsCount];
+        message = [NSString stringWithFormat:@"Added %d new articles! %lu articles might be interesting...", _totalNewCount, _interestingItemsCount];
     }
     
     if (message.length) {
