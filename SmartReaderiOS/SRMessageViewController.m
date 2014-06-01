fbbc0a232086002b18a424ad6c0ae25e56d63785
Ke Sun <kesun.421@gmail.com>
2014-05-31 22:45:55 -0700

    Updated message view to be a slide down view from the top.

--- SmartReaderiOS/SRMessageViewController.m
+++ SmartReaderiOS/SRMessageViewController.m
@@ -38,45 +38,27 @@
                                                object:nil];
     
     if (self) {
-        if (message.length > 20) {
-            _width = 260.0;
-        }
-        else if (message.length > 15){
-            _width = 210.0;
-        }
-        else {
-            _width = 160.0;
-        }
-
-        _height = 40.0;
-        
         if (UIDeviceOrientationIsLandscape(self.interfaceOrientation)) {
-            _x = [UIScreen mainScreen].bounds.size.height;
-            _y = 50.0;
+            _width = [UIScreen mainScreen].bounds.size.height;
+            _height = 54.0;
         }
         else {
-            _x = [UIScreen mainScreen].bounds.size.width;
-            _y = 60.0;
+            _width = [UIScreen mainScreen].bounds.size.width;
+            _height = 68.0;
         }
+        
+        _x = 0.0;
+        _y = -_height;
         
         self.view = [[UIView alloc] initWithFrame:CGRectMake(_x, _y, _width, _height)];
-        self.view.layer.borderColor = [[UIColor grayColor] CGColor];
-        self.view.layer.borderWidth = 0.5;
-        self.view.layer.shadowColor = [[UIColor grayColor] CGColor];
-        self.view.layer.shadowOffset = CGSizeMake(-1.5, 1.5);
-        self.view.layer.shadowRadius = 5;
-        self.view.layer.shadowOpacity = 0.5;
-        self.view.layer.backgroundColor = [[UIColor colorWithRed:249.0f/255.0f green:253.0f/255.0f blue:255.0f/255.0f alpha:1.0] CGColor];
-        self.view.alpha = 0.5;
+        self.view.layer.backgroundColor = [[UIColor colorWithRed:164.0f/255.0f green:194.0f/255.0f blue:244.0f/255.0f alpha:1.0] CGColor];
         
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 22.0, 22.0)];
-        imageView.center = CGPointMake(15.0, _height/2.0 - 1.0);
-        imageView.image = [[UIImage imageNamed:@"info-7.png"] resizeImageToSize:CGSizeMake(22.0, 22.0)];
-        [self.view addSubview:imageView];
-        
-        UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(30.0, 0.0, _width - 30.0 - 10.0, _height)];
-        textField.textAlignment = NSTextAlignmentLeft;
+        UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(0.0, 20.0, _width, _height - 20.0)];
+        textField.textAlignment = NSTextAlignmentCenter;
+        textField.textColor = [UIColor whiteColor];
         textField.text = message;
+        textField.font = [UIFont fontWithName:@"Calibri-Bold" size:20];
+
         [self.view addSubview:textField];
     }
     
@@ -111,7 +93,7 @@
                             options:UIViewAnimationOptionCurveEaseOut
                          animations:^{
                              self.view.alpha = 1.0;
-                             self.view.frame = CGRectMake(_x - _width + 5.0, _y, _width, _height);
+                             self.view.frame = CGRectMake(_x, 0.0, _width, _height);
                          }
                          completion:^(BOOL finished) {
                              
